4355b6c5ecbc168145b967584f30297699bf8341
MartinLSME08 <martinlopezsoto@gmail.com>
2025-05-31 14:15:15 -0300

    Añade clase AlumnoService para operaciones CRUD asíncronas
    Se ha creado la clase interna Services en el espacio de nombres Modelo, incluyendo la implementación de AlumnoService que cumple la interfaz IAlumno. Esta clase gestiona operaciones CRUD asíncronas sobre la entidad Alumno utilizando el contexto Context, con métodos para crear, obtener, actualizar y eliminar alumnos en la base de datos.

---
+++ Entidades/Services.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    internal class Services
+    {
+        public class AlumnoService : IAlumno
+        {
+            private readonly Context _context;
+
+            public AlumnoService(Context context)
+            {
+                _context = context;
+            }
+
+            public async Task<Alumno>? CreateAlumno(Alumno alumno)
+            {
+                _context.Alumnos.Add(alumno);
+                await _context.SaveChangesAsync();
+                return alumno;
+            }
+
+            public async Task<IEnumerable<Alumno>>? GetAllAlumnos()
+            {
+                // no deja asincrono, revisar
+                var usuarios = _context.Alumnos.ToList();
+                return (usuarios);
+            }
+
+            public async Task<Alumno>? GetAlumno(int legajo)
+            {
+                var alumno = await _context.Alumnos.FindAsync(legajo);
+
+                return (alumno);
+            }
+
+            public async Task<Alumno> Update(Alumno alumno)
+            {
+                _context.Alumnos.Update(alumno);
+                await _context.SaveChangesAsync();
+                return alumno;
+            }
+
+            public async Task<Alumno>? Delete(int legajo)
+            {
+                var alumno = await GetAlumno(legajo);
+
+                if (alumno != null)
+                {
+                    _context.Alumnos.Remove(alumno);
+                    await _context.SaveChangesAsync();
+                }
+               
+                return alumno;
+            }
+        }
+    }
+}
