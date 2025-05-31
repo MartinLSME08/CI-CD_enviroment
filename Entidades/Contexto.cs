e1d50b7a34fddd6bd5318ce09c72ed6997e79db3
MartinLSME08 <martinlopezsoto@gmail.com>
2025-05-31 14:14:35 -0300

    Añadir clase Contexto para gestión de Alumnos con EF Core
    Se ha creado la clase Context en Contexto.cs, heredando de DbContext, con un DbSet<Alumno> para gestionar entidades Alumno. El contexto se configura para usar una base de datos en memoria "AlumnosDB". Se añadieron los using necesarios para Entity Framework y colecciones.

---
+++ Entidades/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Context : DbContext
+    {
+        public DbSet<Alumno> Alumnos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("AlumnosDB");
+        }
+    }
+}
