a5ef0a26b517a3ca9af716ca21180d5885882a74
MartinLSME08 <martinlopezsoto@gmail.com>
2025-05-31 14:14:48 -0300

    Añadir interfaz IAlumno en IServices.cs para gestión de Alumno
    Se ha creado la interfaz IAlumno en el archivo IServices.cs bajo el espacio de nombres Modelo. Esta interfaz define métodos asíncronos para crear, obtener (uno o todos), actualizar y eliminar instancias de Alumno, utilizando el legajo como identificador cuando corresponde.

---
+++ Entidades/IServices.cs
@@ -0,0 +1,12 @@
+﻿namespace Modelo
+{
+    public interface IAlumno
+        {
+            Task<Alumno>? CreateAlumno(Alumno alumno);
+            Task<IEnumerable<Alumno>> GetAllAlumnos();
+            Task<Alumno>? GetAlumno(int legajo);
+            Task<Alumno> Update(Alumno alumno);
+            Task<Alumno>? Delete(int legajo);
+
+        }
+}
