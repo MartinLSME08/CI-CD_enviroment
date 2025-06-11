bdfe05abf2e33dba8ed2923f150d557efc7b0292
Martin Alejandro Lopez Soto <106455556+MartinLSME08@users.noreply.github.com>
2025-06-10 21:45:35 -0300

    CT-11: "Introduciendo un Error al método GetAll"

--- Entidades/Services.cs
+++ Entidades/Services.cs
@@ -27,7 +27,6 @@ namespace Modelo
             {
                 // no deja asincrono, revisar
                 var usuarios = _context.Alumnos.ToList();
-                return (usuarios);
             }
 
             public async Task<Alumno>? GetAlumno(int legajo)
